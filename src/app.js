8b66ea67a708352baf6cd8a6ddd127a2fe83bb74
Aelf86 <139387685+Aelf86@users.noreply.github.com>
2024-03-01 18:30:35 +0000

    mi tarea

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //console.log("Hello Rigo from the console!");
+  cargarCarta();
 };
+function cargarCarta() {
+  let palos = ["♦", "♥", "♠", "♣"];
+  let paloRandom = palos[Math.floor(Math.random() * palos.length)];
+  // console.log(paloRandom);
+  let numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+  let numerosRandom = numeros[Math.floor(Math.random() * numeros.length)];
+  // console.log(numerosRandom);
+  document.getElementById("cardTop").innerHTML = paloRandom;
+  document.getElementById("cardBottom").innerHTML = paloRandom;
+  document.getElementById("cardNumber").innerHTML = numerosRandom;
+
+  if (paloRandom == "♥" || paloRandom == "♦") {
+    document.getElementById("cardTop").style.color = "red";
+    document.getElementById("cardBottom").style.color = "red";
+  }
+}
